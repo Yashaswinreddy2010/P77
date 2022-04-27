8a73cc7e9d5e8d9ee24106601b0ceb372e740eb2
Yashaswinreddy2010 <93855479+Yashaswinreddy2010@users.noreply.github.com>
2022-04-27 20:00:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var name = "Yashaswin";
+var last_name = "Reddy";
+var full_name = name.concat(last_name);
+console.log(full_name);
+
+var a = 10;
+var b = 5;
+var add = a+b;
+console.log(add);
+
+var sub = a-b;
+console.log(sub);
+
+var mul = a*b;
+console.log(mul);
+
+var div = a/b;
+console.log(div);
+
